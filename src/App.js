0afda234079c6a0243e9e42fe8806e6be5c74d7a
Yagya Joshi <sevak365@gmail.com>
2022-02-06 13:12:28 -0500

    search engine added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import React from 'react';
 import "./App.css";
 import pokemon from './pokemon.json';
 import PropTypes from 'prop-types';
 
-const PokemonRow = ({ pokemon }) => (
+const PokemonRow = ({ pokemon, onSelect }) => (
   <tr>
       <td>{pokemon.name.english}</td>
-      <td>{pokemon.type.join(',')}</td>
+    <td>{pokemon.type.join(',')}</td>
+    <td>
+      <button onClick={()=> onSelect(pokemon)}>Select!</button>
+    </td>
   </tr> 
 )
 
@@ -15,13 +19,19 @@ PokemonRow.propTypes = {
       english: PropTypes.string,
     }),
     type:PropTypes.arrayOf(PropTypes.string),
-  })
+  }),
+  onSelect: PropTypes.func
 }
 
 function App() {
+  const [filter, setFilter] = React.useState('')
+  const [selectedItem, setSelectedItem] = React.useState(null)
   return (
     <div style={{ margin: "auto", width: 800, paddingTop: "1rem" }}>
       <h1 className="title">Pokemon Search</h1>
+      <input type="text" value={filter} onChange={(e) => setFilter(e.target.value)} />
+      <div style={{ display: 'grid', gridTemplateColumns: '70% 30%', gridColumnGap: '1rem' }}>
+        <div>
       <table width="100%">
         <thead>
           <tr>
@@ -30,11 +40,19 @@ function App() {
           </tr>
         </thead>
         <tbody>
-          {pokemon.map(pokemon => (
-            <PokemonRow pokemon = {pokemon} key={pokemon.id} />
+            {pokemon.filter((pokemon)=>pokemon.name.english.toLowerCase().includes(filter.toLowerCase()))
+          .map(pokemon => (
+            <PokemonRow pokemon = {pokemon} key={pokemon.id} onSelect={(pokemon)=>setSelectedItem(pokemon)}/>
           ))}
         </tbody>
-      </table>
+        </table>
+        </div>
+        {selectedItem && (
+          <div>
+            <h1>{selectedItem.name.english}</h1>
+          </div>
+        )}
+        </div>
     </div>
   );
 }
